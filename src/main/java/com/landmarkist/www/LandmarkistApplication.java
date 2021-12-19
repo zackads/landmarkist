396a0070a1e8808bd0e3f6628bc626e38d9e0f39
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-12-19 18:56:37 +0000

    Initial commit

---
+++ src/main/java/com/landmarkist/www/LandmarkistApplication.java
@@ -0,0 +1,13 @@
+package com.landmarkist.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandmarkistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandmarkistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/landmarkist/www/LandmarkistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landmarkist.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandmarkistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
