0b5f01f02ea2832bb3dd5858708087d177e6b71d
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-12-19 20:36:40 +0000

    Smoke test to load page and see heading

---
+++ src/test/java/com/landmarkist/www/SmokeTest.java
@@ -0,0 +1,39 @@
+package com.landmarkist.www;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public final class SmokeTest {
+
+    private static final BrowserWebDriverContainer<?> chrome = new BrowserWebDriverContainer<>()
+            .withCapabilities(new ChromeOptions());
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeAll
+    static void beforeAll(@Autowired Environment environment) {
+        org.testcontainers.Testcontainers.exposeHostPorts(environment.getProperty("local.server.port", Integer.class));
+        chrome.start();
+    }
+
+    @Test
+    public void indexPageLoads(@Autowired Environment env) {
+        RemoteWebDriver driver = chrome.getWebDriver();
+        String url = "http://host.testcontainers.internal" + ":" + port;
+
+        driver.get(url);
+        assertEquals(1, driver.findElements(By.tagName("h1")).size(), "User can see a single page heading");
+    }
+}
