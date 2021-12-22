db34312e1508f4e569d84afc0d0df065a7054337
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-12-22 11:20:47 +0000

    Define ListedBuilding domain object with minimal data

---
+++ src/main/java/com/landmarkist/www/landmarks/ListedBuilding.java
@@ -0,0 +1,31 @@
+package com.landmarkist.www.landmarks;
+
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListedBuilding {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+
+    @Enumerated
+    private Grade grade;
+
+    enum Grade {
+        ONE, TWO_STAR, TWO
+    }
+}
