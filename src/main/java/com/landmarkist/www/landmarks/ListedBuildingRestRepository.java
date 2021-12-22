d6ec911b300635cad43f6204f0d72ae7f9729026
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-12-22 12:46:15 +0000

    Prevent API from creating/updating a ListedBuilding

--- src/main/java/com/landmarkist/www/landmarks/ListedBuildingRestRepository.java
+++ src/main/java/com/landmarkist/www/landmarks/ListedBuildingRestRepository.java
@@ -3,8 +3,38 @@ package com.landmarkist.www.landmarks;
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
 
 @RepositoryRestResource
 public interface ListedBuildingRestRepository extends PagingAndSortingRepository<ListedBuilding, UUID> {
 
+    @Override
+    @RestResource(exported = false)
+    void deleteById(UUID uuid);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(ListedBuilding entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends UUID> uuids);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends ListedBuilding> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ListedBuilding> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ListedBuilding> Iterable<S> saveAll(Iterable<S> entities);
 }
