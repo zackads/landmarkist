7abdc12db175d14fc3fa2baacf8961302c96774d
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-02-05 22:31:32 +0000

    Differentiate by grade of building

--- src/main/reactapp/src/App.tsx
+++ src/main/reactapp/src/App.tsx
@@ -20,15 +20,20 @@ function App() {
                 ref={mapRef}
                 id="map"
                 mapboxAccessToken={"pk.eyJ1IjoiemFja2FkcyIsImEiOiJjazZ3bnYyajAwOWp5M2htYW9qemQ2dXRyIn0.GbE76MBYsJpDdStQgE_YHw"}
-                initialViewState={{longitude: -2.59, latitude: 51.45}}
+                initialViewState={{longitude: -2.59, latitude: 51.45, zoom: 14}}
                 style={{height: "100vh", width: "100vw"}}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
+                mapStyle="mapbox://styles/mapbox/light-v10"
                 onLoad={handleMove}
                 onMove={handleMove}
             >
                 <Source id="listed-buildings" type="geojson"
                         data={landmarks}>
-                    <Layer id="point" type="circle"/>
+                    <Layer id="grade-i" type="circle" filter={['==', "grade", "I"]}
+                           paint={{"circle-radius": 8, "circle-color": "#B80C09", "circle-opacity": 0.9}}/>
+                    <Layer id="grade-ii*" type="circle" filter={['==', "grade", "II*"]}
+                           paint={{"circle-radius": 5, "circle-color": "#0B4F6C", "circle-opacity": 0.9}}/>
+                    <Layer id="grade-ii" type="circle" filter={['==', "grade", "II"]}
+                           paint={{"circle-radius": 4, "circle-color": "#01BAEF", "circle-opacity": 0.9}}/>
                 </Source>
 
             </Map>
