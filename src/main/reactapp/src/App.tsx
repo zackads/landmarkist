57a1422834f142c37ce47210ec44edbcc28a0824
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-02-05 21:42:54 +0000

    Map displays listed buildings as small circles

--- src/main/reactapp/src/App.tsx
+++ src/main/reactapp/src/App.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Map from "react-map-gl";
+import Map, {Layer, MapProvider, MapRef, Source} from "react-map-gl";
+import {MapboxMap} from "react-map-gl/src/types/index";
+import {FeatureCollection} from "geojson";
 
 function App() {
+    const [landmarks, setLandmarks] = useState<FeatureCollection | string>(emptyGeoJson);
+
+    const mapRef = React.useRef<MapRef>(null);
+
     return (
-        <Map
-            mapboxAccessToken={"pk.eyJ1IjoiemFja2FkcyIsImEiOiJjazZ3bnYyajAwOWp5M2htYW9qemQ2dXRyIn0.GbE76MBYsJpDdStQgE_YHw"}
-            initialViewState={{longitude: -2.59, latitude: 51.45}}
-            style={{width: '100vw', height: '100vh'}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-        />
+        <MapProvider>
+            <Map
+                ref={mapRef}
+                id="map"
+                mapboxAccessToken={"pk.eyJ1IjoiemFja2FkcyIsImEiOiJjazZ3bnYyajAwOWp5M2htYW9qemQ2dXRyIn0.GbE76MBYsJpDdStQgE_YHw"}
+                initialViewState={{longitude: -2.59, latitude: 51.45}}
+                style={{height: "100vh", width: "100vw"}}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                onMove={() =>
+                    setLandmarks(`/api/listedBuildings/search/findAllInPolygon?polygon=${viewport(mapRef.current!.getMap())}`)
+                }
+                onLoad={() =>
+                    setLandmarks(`/api/listedBuildings/search/findAllInPolygon?polygon=${viewport(mapRef.current!.getMap())}`)
+                }
+            >
+                <Source id="listed-buildings" type="geojson"
+                        data={landmarks}>
+                    <Layer id="point" type="circle"/>
+                </Source>
+
+            </Map>
+        </MapProvider>
     );
+}
+
+const viewport = (map: MapboxMap | null): string => {
+    if (map == null) return "";
+
+    const bounds = map.getBounds();
+
+    return [
+        bounds.getNorthEast().toArray().toString(),
+        bounds.getSouthEast().toArray().toString(),
+        bounds.getSouthWest().toArray().toString(),
+        bounds.getNorthWest().toArray().toString(),
+        bounds.getNorthEast().toArray().toString(),
+    ].join(",");
+};
+
+const emptyGeoJson: FeatureCollection = {
+    "type": "FeatureCollection", "features": []
 }
 
 export default App;
